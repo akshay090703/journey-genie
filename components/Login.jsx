a321180f3f7fa7898ad8dd2833edfe79bf0bd794
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-07-11 00:04:43 +0530

    signin and signup interface

---
+++ components/Login.jsx
@@ -0,0 +1,75 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
+
+export default function Login() {
+  const router = useRouter();
+
+  return (
+    <View>
+      <Image
+        source={require("../assets/images/login.jpg")}
+        style={{ width: "100%", height: 520 }}
+      />
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+            marginTop: 10,
+          }}
+        >
+          Journey Genie
+        </Text>
+
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop: 20,
+          }}
+        >
+          Discover your next adventure effortlessly. Personalized itineraries at
+          your fingertips. Travel smarted with Ai-driven insights.
+        </Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("auth/sign-in")}
+        >
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 17,
+            }}
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    height: "100%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 25,
+  },
+  button: {
+    backgroundColor: Colors.PRIMARY,
+    padding: 15,
+    borderRadius: 99,
+    marginTop: "25%",
+  },
+});
